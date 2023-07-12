79a216c13e606dae8aa854c268cd631a0cda3913
Бехруз Шухратов <@corp.itstep>
2023-07-12 21:16:21 +0500

    i done task2

---
+++ main.py
@@ -0,0 +1,8 @@
+import datetime
+current_year = datetime.datetime.now().year
+print(current_year)
+i = int(input())
+if (i % 4 == 0 or i % 400 == 0) and i % 100 != 0:
+    print("YES")
+else:
+    print("NO")
